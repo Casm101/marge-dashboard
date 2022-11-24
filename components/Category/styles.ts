f91ca7aa262290eda2e127610a4810e0fbdf14c2
Christian Smith Mantas <casm202@gmail.com>
2022-11-24 11:45:54 +0100

    Update: service card and page styles

--- components/Category/styles.ts
+++ components/Category/styles.ts
@@ -3,7 +3,8 @@ import styled from "styled-components";
 export const StyledCategory = styled.div`
   display: flex;
   flex-direction: column;
-  width: 30%;
+  min-width: 300px;
+  max-width: 30%;
 
   h2 {
 
